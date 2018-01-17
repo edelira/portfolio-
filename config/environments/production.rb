453e4546ffbfcf9eae8a9f613ef34de38848bd80
EC2 Default User <ec2-user@ip-172-31-32-71.us-west-2.compute.internal>
2018-01-17 16:10:47 +0000

    fixed production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,16 @@
 Rails.application.configure do
+# For mail_form gem
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors= true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                      587,
+    domain:                 'gmail.com',
+    user_name:             ENV["GMAIL_USERNAME"],
+    password:              ENV["GMAIL_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true  }  # Settings specified here will take precedence over those in config/application.rb.
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
